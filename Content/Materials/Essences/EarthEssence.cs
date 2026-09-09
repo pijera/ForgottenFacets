b7997b074f3505d5b9afd62364f27db2cb62a14d
pijera <petar200706@gmail.com>
2026-09-09 21:16:14 +0200

    3 essenca doata i dropovi doati
    dodati jos 4 preostala i napraviti sve recepte za gemove i implementirati aquamarine i izabciti ancient ice barove iz igrice i resprite ancient ice orea i promenti ancient ice recepte (oruzija i armor)

---
+++ Content/Materials/Essences/EarthEssence.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenFacets.Content.Materials.Essences
+{
+    internal class EarthEssence : ModItem
+    {
+        public override string Texture => "ForgottenFacets/Assets/Materials/Essences/EarthEssence";
+
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 25;
+            ItemID.Sets.SortingPriorityMaterials[Type] = 60;
+
+            ItemID.Sets.AnimatesAsSoul[Type] = true;
+            Main.RegisterItemAnimation(Type, new DrawAnimationVertical(8, 23));
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+
+            Item.sellPrice(silver: 1, copper: 50);
+            Item.rare = ItemRarityID.Blue;
+            Item.maxStack = 9999;
+        }
+
+        public override void PostUpdate()
+        {
+            Lighting.AddLight(Item.Center, Color.WhiteSmoke.ToVector3() * 0.55f * Main.essScale); // Makes this item glow when thrown out of inventory.
+        }
+
+        /*public override void Update(ref float gravity, ref float maxFallSpeed)
+        {
+            gravity = 0f;
+            maxFallSpeed = 0f;
+
+            Item.velocity.Y *= 0.95f;
+        }
+        public override void OnSpawn(IEntitySource source)
+        {
+            Item.velocity.Y -= 3f;
+        }
+
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            Texture2D texture = TextureAssets.Item[Type].Value;
+
+            int frameCount = 23;
+
+            int frameHeight = texture.Height / frameCount;
+            int frame = (int)(Main.GlobalTimeWrappedHourly * 8f) % frameCount;
+
+            Rectangle sourceRectangle = new Rectangle(0, frameHeight * frame, texture.Width, frameHeight);
+            Vector2 origin = new Vector2(sourceRectangle.Width / 2f, sourceRectangle.Height / 2f);
+
+
+            float bob = (float)Math.Sin(Main.GlobalTimeWrappedHourly * 3f) * 3f;
+
+            Vector2 position = Item.position + Item.Size / 2f;
+            position.Y += bob;
+
+            spriteBatch.Draw(texture, position - Main.screenPosition, sourceRectangle, lightColor, rotation, origin, scale, SpriteEffects.None, 0f);
+
+            for (int i = 0; i < 2; i++)
+            {
+                float offset = MathHelper.TwoPi * i / 4f;
+
+                Vector2 glowOffset = new Vector2((float)Math.Cos(offset) * 2f, (float)Math.Sin(offset) * 2f);
+                spriteBatch.Draw(texture, position - Main.screenPosition + glowOffset, sourceRectangle, new Color(255, 255, 255, 0) * 0.25f, rotation, origin, scale, SpriteEffects.None, 0f);
+            }
+
+            return false;
+        }*/
+
+    }
+}
---
+++ Core/VanillaEditedDrops.cs
@@ -0,0 +1,235 @@
+﻿using ForgottenFacets.Content.Materials.Essences;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ForgottenFacets.Core
+{
+    internal class VanillaEditedDrops : GlobalNPC
+    {
+        private static readonly HashSet<int> WaterEssenceDroppers = new()//water essence,sapphire
+        {
+            //pre-hardmode
+            NPCID.BlueJellyfish,
+            NPCID.Crab,
+            NPCID.ZombieEskimo,NPCID.ArmedZombieEskimo,
+            NPCID.IceBat,
+            NPCID.IceSlime,
+            NPCID.BlueJellyfish,NPCID.PinkJellyfish,NPCID.GreenJellyfish, NPCID.BloodJelly,NPCID.FungoFish,
+            NPCID.Piranha,
+            NPCID.SeaSnail,
+            NPCID.Shark,
+            NPCID.SnowFlinx,
+            NPCID.SpikedIceSlime,
+            NPCID.Squid,
+            NPCID.UndeadViking,
+
+            //hardmode
+            NPCID.AnglerFish,
+            NPCID.Arapaima,
+            NPCID.ArmoredViking,
+            NPCID.BloodFeeder,
+            NPCID.IceElemental,
+            NPCID.IceMimic,
+            NPCID.IceTortoise,
+            NPCID.IcyMerman,
+            NPCID.PigronCorruption,NPCID.PigronCrimson,NPCID.PigronHallow,
+            NPCID.ZombieMerman
+
+        };
+
+        public static readonly HashSet<int> FireEssenceDroppers = new()//fire essence,ruby
+        {
+            //pre-hardmode
+            NPCID.BoneSerpentHead,
+            NPCID.Demon,
+            NPCID.FireImp,
+            NPCID.Hellbat,
+            NPCID.LavaSlime,
+            NPCID.VoodooDemon,
+            NPCID.MeteorHead,
+
+            //hardmode
+
+            NPCID.HellArmoredBones,NPCID.HellArmoredBonesMace,NPCID.HellArmoredBonesSpikeShield,NPCID.HellArmoredBonesSword,
+            NPCID.Lavabat,
+            NPCID.RedDevil,
+            534,
+        };
+
+        public static readonly HashSet<int> EarthEssenceDroppers = new()//earth essence,topaz
+        {
+            //pre-hardmode
+            NPCID.BlackSlime,
+            NPCID.CaveBat,
+            NPCID.CochinealBeetle,
+            NPCID.Crawdad,NPCID.Crawdad2,
+            NPCID.CyanBeetle,
+            NPCID.GiantShelly,NPCID.GiantShelly2,
+            NPCID.GiantWormHead,
+            NPCID.GraniteFlyer,
+            NPCID.GraniteGolem,
+            481,//hoplite
+            NPCID.MotherSlime,
+            NPCID.Nymph,
+            NPCID.Salamander,NPCID.Salamander2,NPCID.Salamander3,NPCID.Salamander4,NPCID.Salamander5,NPCID.Salamander6,NPCID.Salamander7,NPCID.Salamander8,NPCID.Salamander9,
+            NPCID.Skeleton,NPCID.BoneThrowingSkeleton,NPCID.SmallSkeleton,NPCID.BigSkeleton,
+            NPCID.HeadacheSkeleton,NPCID.BoneThrowingSkeleton2,NPCID.SmallHeadacheSkeleton,NPCID.BigHeadacheSkeleton,
+            NPCID.MisassembledSkeleton,NPCID.BoneThrowingSkeleton3,NPCID.SmallMisassembledSkeleton, NPCID.BigMisassembledSkeleton,
+            NPCID.PantlessSkeleton,NPCID.BoneThrowingSkeleton4,NPCID.BigPantlessSkeleton,NPCID.SmallPantlessSkeleton,
+            NPCID.UndeadMiner,
+            NPCID.WallCreeper,
+
+            //hardmode
+            NPCID.ArmoredSkeleton, NPCID.HeavySkeleton,
+            NPCID.BlackRecluse,
+            NPCID.GiantBat,
+            NPCID.Medusa,
+            NPCID.RockGolem,
+            NPCID.SkeletonArcher,
+            NPCID.ToxicSludge
+
+
+        };
+        public static readonly HashSet<int> LifeEssenceDroppers = new()//life essence,emerald
+        {
+            //pre-hardmode
+            NPCID.AnomuraFungus,
+            NPCID.DoctorBones,
+            NPCID.FungiBulb,
+            NPCID.Hornet,NPCID.LittleStinger,NPCID.BigStinger,NPCID.HornetFatty,NPCID.LittleHornetFatty,NPCID.BigHornetFatty,NPCID.HornetHoney,NPCID.LittleHornetHoney,NPCID.BigHornetHoney,NPCID.HornetLeafy,NPCID.LittleHornetLeafy,
+            NPCID.BigHornetLeafy,NPCID.HornetSpikey,NPCID.LittleHornetSpikey,NPCID.BigHornetSpikey,NPCID.HornetStingy,NPCID.BigHornetStingy,NPCID.LittleHornetStingy,
+            NPCID.JungleBat,
+            NPCID.JungleSlime,
+            NPCID.LacBeetle,
+            NPCID.ManEater,
+            NPCID.MushiLadybug,
+            NPCID.Snatcher,
+            NPCID.SpikedJungleSlime,
+            NPCID.SporeBat,
+            NPCID.SporeSkeleton,
+            NPCID.ZombieMushroom,NPCID.ZombieMushroomHat,
+
+            //hardmode
+            NPCID.AngryTrapper,
+            NPCID.Derpling,
+            NPCID.FlyingSnake,
+            NPCID.GiantFlyingFox,
+            NPCID.GiantTortoise,
+            NPCID.JungleCreeper,
+            NPCID.Lihzahrd,
+            NPCID.MossHornet,NPCID.BigMossHornet,NPCID.GiantMossHornet,NPCID.LittleMossHornet,NPCID.TinyMossHornet,
+            NPCID.Moth
+
+        };
+        public static readonly HashSet<int> StormEssenceDroppers = new()//storm essence,diamond
+        {
+            //pre-hardmode
+            NPCID.Harpy,
+            NPCID.Dandelion,
+
+            //hardmode
+            NPCID.WyvernHead,
+            NPCID.AngryNimbus,
+            NPCID.FlyingFish,
+            NPCID.ZombieRaincoat,
+            NPCID.UmbrellaSlime
+        };
+        public static readonly HashSet<int> ArcaneEssenceDroppers = new()//arcane essence,amethyst
+        {
+            //pre-hardmode
+            NPCID.AngryBones, NPCID.AngryBonesBig, NPCID.AngryBonesBigHelmet, NPCID.AngryBonesBigMuscle, NPCID.BigBoned, NPCID.ShortBones,
+            NPCID.CursedSkull,
+            NPCID.DarkCaster,
+            NPCID.DungeonGuardian,
+            NPCID.DungeonSlime,
+            NPCID.Ghost,
+            NPCID.Gnome,
+            NPCID.Tim,
+            NPCID.GoblinSorcerer,
+
+            //hardmode
+            NPCID.BlueArmoredBones,NPCID.BlueArmoredBonesMace,NPCID.BlueArmoredBonesNoPants,NPCID.BlueArmoredBonesSword,
+            NPCID.BoneLee,
+            NPCID.BigMimicCorruption,NPCID.BigMimicCrimson,NPCID.BigMimicHallow,
+            NPCID.CultistArcherBlue,
+            NPCID.DiabolistRed,NPCID.DiabolistWhite,
+            NPCID.DungeonSpirit,
+            NPCID.GiantCursedSkull,
+            NPCID.HoppinJack,
+            NPCID.HellArmoredBones,NPCID.HellArmoredBonesMace,NPCID.HellArmoredBonesSpikeShield,NPCID.HellArmoredBonesSword,
+            NPCID.CultistDevote,
+            NPCID.Necromancer,NPCID.NecromancerArmored,
+            NPCID.Paladin,
+            NPCID.RaggedCaster,NPCID.RaggedCasterOpenCoat,
+            NPCID.RuneWizard,
+            NPCID.RustyArmoredBonesAxe,NPCID.RustyArmoredBonesFlail,NPCID.RustyArmoredBonesSwordNoArmor,NPCID.RustyArmoredBonesSword,
+            NPCID.SkeletonCommando,
+            NPCID.SkeletonSniper,
+            NPCID.TacticalSkeleton,
+            NPCID.GoblinSummoner
+        };
+        public static readonly HashSet<int> AncientEssenceDroppers = new()//ancient essence,amber
+        {
+            //pre-hardmode
+            NPCID.Antlion,
+            NPCID.GiantWalkingAntlion,NPCID.WalkingAntlion,
+            NPCID.FlyingAntlion, NPCID.GiantFlyingAntlion,
+            NPCID.SandSlime,
+            NPCID.TombCrawlerHead,
+
+            //hardmode
+            532,//Basilisk
+            NPCID.BloodMummy,NPCID.DarkMummy,NPCID.Mummy,NPCID.LightMummy,
+            NPCID.DesertDjinn,
+            NPCID.DesertGhoul, NPCID.DesertGhoulCorruption, NPCID.DesertGhoulCrimson, NPCID.DesertGhoulHallow,
+            NPCID.DuneSplicerHead,
+            NPCID.DesertLamiaDark,NPCID.DesertLamiaLight,
+            530,531,//skorpijon bumass
+            NPCID.Tumbleweed,
+            NPCID.SandShark,NPCID.SandsharkCorrupt,NPCID.SandsharkCrimson,NPCID.SandsharkHallow
+        };
+
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            if (WaterEssenceDroppers.Contains(npc.type))
+            {
+                IItemDropRule classicRule = ItemDropRule.Common(ModContent.ItemType<WaterEssence>(), 4, 1, 3);
+                IItemDropRule expertRule = ItemDropRule.Common(ModContent.ItemType<WaterEssence>(), 3, 1, 3);
+
+                IItemDropRule difficultyScaling = new DropBasedOnExpertMode(classicRule, expertRule);
+
+                npcLoot.Add(difficultyScaling);
+            }
+
+            if (FireEssenceDroppers.Contains(npc.type))
+            {
+                IItemDropRule classicRule = ItemDropRule.Common(ModContent.ItemType<FireEssence>(), 4, 1, 3);
+                IItemDropRule expertRule = ItemDropRule.Common(ModContent.ItemType<FireEssence>(), 3, 1, 3);
+
+                IItemDropRule difficultyScaling = new DropBasedOnExpertMode(classicRule, expertRule);
+
+                npcLoot.Add(difficultyScaling);
+            }
+
+            if (EarthEssenceDroppers.Contains(npc.type))
+            {
+                IItemDropRule classicRule = ItemDropRule.Common(ModContent.ItemType<EarthEssence>(), 4, 1, 3);
+                IItemDropRule expertRule = ItemDropRule.Common(ModContent.ItemType<EarthEssence>(), 3, 1, 3);
+
+                IItemDropRule difficultyScaling = new DropBasedOnExpertMode(classicRule, expertRule);
+
+                npcLoot.Add(difficultyScaling);
+            }
+
+        }
+    }
+}
